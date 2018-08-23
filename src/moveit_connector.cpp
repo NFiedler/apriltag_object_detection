a7541a14ee8033febd687a7aef334f80b7a1c7c4
Niklas Fiedler <git@nfiedler.de>
2018-08-23 18:45:58 +0200

    preventing visual detection of attached objects [compiles, untested]

--- src/moveit_connector.cpp
+++ src/moveit_connector.cpp
@@ -69,16 +69,19 @@ MoveitConnector::MoveitConnector() : nh_() {
 void MoveitConnector::objectsCallback(const visualization_msgs::MarkerArray &msg){
   moveit_msgs::CollisionObject collision_object;
   for(int i = 0; i < msg.markers.size(); i++) {
-    // TODO: do not update attached objects
     if(!markerMsgToCollisionObjectMsg(msg.markers[i], "/odom_combined", collision_object)) {
-        ROS_ERROR_STREAM("Marker could not get transformed to a CollisionObject!");
-        continue;
+      ROS_ERROR_STREAM("Marker could not get transformed to a CollisionObject!");
+      continue;
     }
+    if(!psi_->getAttachedObjects(std::vector<std::string>({static_cast<std::string>(collision_object.id)})).empty()){ // just checking if the object is an attached collision object
+      ROS_DEBUG_STREAM("Object detection detected attached object: " << collision_object.id);
+      continue;
+    }
+    // TODO: use MOVE, when the object exists already. (Only the pose in the CollisionObject)
     collision_object.operation = collision_object.ADD;
-
     if(!psi_->applyCollisionObject(collision_object)) {
-        ROS_ERROR_STREAM("Failed to add CollisionObject to planning scene!");
-        continue;
+      ROS_ERROR_STREAM("Failed to add CollisionObject to planning scene!");
+      continue;
     }
   }
 
